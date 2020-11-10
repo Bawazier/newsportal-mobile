14e2c75a5d8a179f73ee16fbf2a9675ef1c972d1
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-11-10 23:57:54 +0700

    test main screens

--- App/index.js
+++ App/index.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 import {Container, Header} from 'native-base';
-import Main from './screens/Main';
+// import Main from './screens/Main';
+import Topics from './screens/Topics';
 
 export class App extends Component {
   render() {
     return (
       <Container>
         <Header />
-        <Main />
+        {/* <Main /> */}
+        <Topics />
       </Container>
     );
   }

d1cecc55f87c25e91f1b9dba5d4710aae81ac324
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-11-10 21:43:31 +0700

    created global style

---
+++ App/styles/index.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components/native';
+import {Text, Content} from 'native-base';
+
+export const StyledContent = styled(Content)`
+  padding: 0 10px;
+  margin: 20px 0;
+`;
+
+export const TextStyled = styled(Text)`
+  font-weight: bold;
+  font-size: 12px;
+  color: #292929;
+`;
+export const TextTitleStyled = styled(Text)`
+  font-weight: bold;
+  font-size: 16px;
+  color: #292929;
+`;
+export const TextSecondaryStyled = styled(Text)`
+  font-size: 16px;
+  color: #292929;
+`;
+export const TextButtonStyled = styled(Text)`
+  font-size: 14px;
+  color: #ffffff;
+`;
+export const TextButtonTransparentStyled = styled(Text)`
+  font-size: 14px;
+  color: #1a8917;
+`;
